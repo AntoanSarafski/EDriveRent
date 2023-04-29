62f49f2e26b64e382351872fdc4c108f017b6bf7
antoansarafski@protonmail.com <antoansarafski@protonmail.com>
2023-04-29 13:44:44 +0300

    Commit with RegisterUser method and constructor in Controller class.

--- EDriveRent/Core/Controller.cs
+++ EDriveRent/Core/Controller.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Models;
+using EDriveRent.Repositories.Contracts;
+using EDriveRent.Repositories;
+using EDriveRent.Utilities.Messages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EDriveRent.Core.Contracts;
 
 namespace EDriveRent.Core
 {
-    internal class Controller
+    public class Controller : IController
     {
+
+        private readonly IRepository<IUser> users;
+        private readonly IRepository<IVehicle> vehicles;
+        private readonly IRepository<IRoute> routes;
+
+        public Controller()
+        {
+            users = new UserRepository();
+            vehicles = new VehicleRepository();
+            routes = new RouteRepository();
+        }
+
+
+
+        public string RegisterUser(string firstName, string lastName, string drivingLicenseNumber)
+        {
+            if (users.FindById(drivingLicenseNumber) != null)
+            {
+                return string.Format(OutputMessages.UserWithSameLicenseAlreadyAdded, drivingLicenseNumber);
+            }
+            IUser user = new User(firstName, lastName, drivingLicenseNumber);
+
+            users.AddModel(user);
+
+            return string.Format(OutputMessages.UserSuccessfullyAdded, firstName, lastName, drivingLicenseNumber);
+        }
+
+
+        public string AllowRoute(string startPoint, string endPoint, double length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string MakeTrip(string drivingLicenseNumber, string licensePlateNumber, string routeId, bool isAccidentHappened)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public string RepairVehicles(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UploadVehicle(string vehicleType, string brand, string model, string licensePlateNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UsersReport()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
